5bcc3dd81a92c09116c13c006bdb55548b3ea5a2
magnanimox <tiagoeds96@gmail.com>
2022-11-02 12:15:29 -0300

    up

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,75 @@
 import { Request, Response } from "express";
 import { EpisodesTp } from "../models/TpEpisodes";
 import { EpisodesAb } from "../models/AbEpisodes";
-import { monthName, cardTp, cardAb } from "../helpers/index";
+import {
+    monthName,
+    prevMonthName,
+    nextMonthName,
+    cardTp,
+    cardAb,
+    cardAbPrevMonth,
+    cardAbNextMonth,
+    cardTpPrevMonth,
+    cardTpNextMonth,
+} from "../helpers/index";
 import dayjs from "dayjs";
 
+export const login = async (req: Request, res: Response) => {};
+
 export const inicio = async (req: Request, res: Response) => {
-    const episodeTpToShow = await cardTp();
-    const episodeAbToShow = await cardAb();
-    let episodeWeekDisplay = dayjs(episodeTpToShow.showAt).format(
-        "DD/MMM/YYYY"
-    );
+    let episodeTpToShow = await cardTp();
+    let episodeAbToShow = await cardAb();
+    let prevMonthLink = "http://localhost:3000/prev/" + prevMonthName;
+    let nextMonthLink = "http://localhost:3000/next/" + nextMonthName;
+    let selectInicio = "selected";
 
     res.render("pages/inicio", {
         monthName,
         episodeTpToShow,
         episodeAbToShow,
-        episodeWeekDisplay,
+        prevMonthName,
+        nextMonthName,
+        prevMonthLink,
+        nextMonthLink,
+        selectInicio,
+    });
+};
+
+export const prevmonth = async (req: Request, res: Response) => {
+    let episodeTpToShow = await cardTpPrevMonth();
+    let episodeAbToShow = await cardAbPrevMonth();
+    let prevMonthLink = "http://localhost:3000/prev/" + prevMonthName;
+    let nextMonthLink = "http://localhost:3000/next/" + nextMonthName;
+    let selectInicio = "selected";
+
+    res.render("pages/prevmonth", {
+        monthName,
+        episodeTpToShow,
+        episodeAbToShow,
+        prevMonthName,
+        nextMonthName,
+        prevMonthLink,
+        nextMonthLink,
+        selectInicio,
+    });
+};
+
+export const nextmonth = async (req: Request, res: Response) => {
+    let episodeTpToShow = await cardTpNextMonth();
+    let episodeAbToShow = await cardAbNextMonth();
+    let prevMonthLink = "http://localhost:3000/prev/" + prevMonthName;
+    let nextMonthLink = "http://localhost:3000/next/" + nextMonthName;
+    let selectInicio = "selected";
+
+    res.render("pages/nextmonth", {
+        monthName,
+        episodeTpToShow,
+        episodeAbToShow,
+        prevMonthName,
+        nextMonthName,
+        prevMonthLink,
+        nextMonthLink,
+        selectInicio,
     });
 };
 
@@ -29,9 +83,11 @@ export const recursos = async (req: Request, res: Response) => {
 
 export const aventura = async (req: Request, res: Response) => {
     let episodes = await EpisodesAb.findAll();
+    let selectAssista = "selected";
 
     res.render("pages/aventura-biblica", {
         episodes,
+        selectAssista,
     });
 };
 
@@ -39,9 +95,11 @@ export const temp1 = async (req: Request, res: Response) => {
     let episodes = await EpisodesTp.findAll({
         where: { temp: 1 },
     });
+    let selectAssista = "selected";
 
     res.render("pages/1temporada", {
         episodes,
+        selectAssista,
     });
 };
 
@@ -49,8 +107,10 @@ export const temp2 = async (req: Request, res: Response) => {
     let episodes = await EpisodesTp.findAll({
         where: { temp: 2 },
     });
+    let selectAssista = "selected";
 
     res.render("pages/2temporada", {
         episodes,
+        selectAssista,
     });
 };
