50e6d2883015392f07a86ad9c2186468b056f376
magnanimox <tiagoeds96@gmail.com>
2022-12-07 17:37:06 -0300

    danger

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -21,7 +21,7 @@ export const { signin, signup } = {
 
         // Verificando o e-mail
         const user = await User.findOne({ where: { email: data.email } });
-        if (!user) {
+        if (!user || null) {
             danger = true;
             res.render("pages/signin", { danger });
             return;
@@ -29,7 +29,7 @@ export const { signin, signup } = {
 
         // Verificando a senha
         const match = await bcrypt.compare(data.password, user.passwordHash);
-        if (!match) {
+        if (!match || null) {
             danger = true;
             res.render("pages/signin", { danger });
             return;
