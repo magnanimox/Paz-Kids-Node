cbfee60f1af92ca880c3a2da73c033a27fe7c6a8
magnanimox <tiagoeds96@gmail.com>
2022-12-07 19:27:33 -0300

    httpS on cities and churches

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -34,7 +34,7 @@ function handleCity(event) {
 
 // JSON's
 async function getCities() {
-    await fetch("http://pazkids.com.br/user/cities")
+    await fetch("https://pazkids.com.br/user/cities")
         .then((data) => {
             return data.json();
         })
@@ -47,7 +47,7 @@ async function getCities() {
 }
 
 async function getChurches() {
-    await fetch("http://pazkids.com.br/user/churches")
+    await fetch("https://pazkids.com.br/user/churches")
         .then((data) => {
             return data.json();
         })
