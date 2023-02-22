02a03f4fa771614e96f861c485bb1a12bcc7f399
magnanimox <tiagoeds96@gmail.com>
2023-02-22 12:02:29 -0300

    authController password smtp

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -168,7 +168,7 @@ export const { forgotGet, forgotPost } = {
                 port: 587,
                 auth: {
                     user: "pazkidsonline@gmail.com",
-                    pass: "saadmdxtmmdeovei",
+                    pass: "janxepgjufyoueov",
                 },
             });
 
