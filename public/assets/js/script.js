6923113469e4d605841ba538818d2c7159df52d2
magnanimox <tiagoeds96@gmail.com>
2023-03-03 13:20:21 -0300

    css fix

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -89,15 +89,3 @@ function closeModal() {
         c(".modal-login").style.display = "none";
     }, 500);
 }
-
-function addcss(css) {
-    var d = new Date();
-    var n = d.getTime();
-    var head = document.getElementsByTagName("head")[0];
-    var s = document.createElement("link");
-    s.setAttribute("rel", "stylesheet");
-    s.setAttribute("href", css + "?v=" + n);
-    head.appendChild(s);
-}
-
-addcss("/css/style.css");
