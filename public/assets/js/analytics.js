846eec4269492431b1699c724f2933942ba1b604
magnanimox <tiagoeds96@gmail.com>
2023-05-01 16:10:12 -0300

    debug mod test

--- public/assets/js/analytics.js
+++ public/assets/js/analytics.js
@@ -3,23 +3,23 @@ function Ab_Download_Counter() {
 }
 
 function Ab_Materiais_Counter() {
-    gtag("event", "ab-materiais-counter");
+    gtag("event", "ab-materiais-counter", { "debug-mode": true });
 }
 
 function Tp_Download_Counter() {
-    gtag("event", "tp-download-counter");
+    gtag("event", "tp-download-counter", { "debug-mode": true });
 }
 
 function Tp_Slides_Counter() {
-    gtag("event", "tp-slides-counter");
+    gtag("event", "tp-slides-counter", { "debug-mode": true });
 }
 
 function Tp_Youtube_Counter() {
-    gtag("event", "tp-youtube-counter");
+    gtag("event", "tp-youtube-counter", { "debug-mode": true });
 }
 
 function Tp_Lifekids_Counter() {
-    gtag("event", "tp-lifekids-counter");
+    gtag("event", "tp-lifekids-counter", { "debug-mode": true });
 }
 
 // document.getElementById("ab-download").addEventListener("click", function () {
