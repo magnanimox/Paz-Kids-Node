e94b60a60af676028085a48b46a3b77c88f44109
magnanimox <tiagoeds96@gmail.com>
2023-05-01 17:29:13 -0300

    debug mod test

--- public/assets/js/analytics.js
+++ public/assets/js/analytics.js
@@ -1,47 +1,23 @@
-// window.ab_Download_Counter = function () {
-//     gtag("event", "ab-download-counter");
-// }
+document.getElementById("ab-download").addEventListener("click", function () {
+    gtag("event", "ab-download-counter", { "debug-mode": true });
+});
 
-// window.ab_Materiais_Counter = function() {
-//     gtag("event", "ab-materiais-counter", { "debug-mode": true });
-// }
+document.getElementById("ab-materiais").addEventListener("click", function () {
+    gtag("event", "ab-download-counter", { "debug-mode": true });
+});
 
-// window.tp_Download_Counter() = function() {
-//     gtag("event", "tp-download-counter", { "debug-mode": true });
-// }
+document.getElementById("tp-download").addEventListener("click", function () {
+    gtag("event", "tp-download-counter", { "debug-mode": true });
+});
 
-// window.tp_Slides_Counter() = function() {
-//     gtag("event", "tp-slides-counter", { "debug-mode": true });
-// }
+document.getElementById("tp-slides").addEventListener("click", function () {
+    gtag("event", "tp-slides-counter", { "debug-mode": true });
+});
 
-// window.tp_Youtube_Counter() = function() {
-//     gtag("event", "tp-youtube-counter", { "debug-mode": true });
-// }
+document.getElementById("tp-youtube").addEventListener("click", function () {
+    gtag("event", "tp-youtube-counter", { "debug-mode": true });
+});
 
-// window.tp_Lifekids_Counter() = function() {
-//     gtag("event", "tp-lifekids-counter", { "debug-mode": true });
-// }
-
-// document.getElementById("ab-download").addEventListener("click", function () {
-//     gtag("event", "ab-download-counter");
-// });
-
-// document.getElementById("ab-materiais").addEventListener("click", function () {
-//     gtag("event", "ab-download-counter");
-// });
-
-// document.getElementById("tp-download").addEventListener("click", function () {
-//     gtag("event", "tp-download-counter");
-// });
-
-// document.getElementById("tp-slides").addEventListener("click", function () {
-//     gtag("event", "tp-slides-counter");
-// });
-
-// document.getElementById("tp-youtube").addEventListener("click", function () {
-//     gtag("event", "tp-youtube-counter");
-// });
-
-// document.getElementById("tp-lifekids").addEventListener("click", function () {
-//     gtag("event", "tp-lifekids-counter");
-// });
+document.getElementById("tp-lifekids").addEventListener("click", function () {
+    gtag("event", "tp-lifekids-counter", { "debug-mode": true });
+});
