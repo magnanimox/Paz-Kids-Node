dc56a760d4df8e1926abc979543f747f4a220dc7
magnanimox <tiagoeds96@gmail.com>
2023-05-01 14:43:13 -0300

    analityc debug mode 2

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -89,27 +89,3 @@ function closeModal() {
         c(".modal-login").style.display = "none";
     }, 500);
 }
-
-document
-    .getElementsByClassName("download-counter")
-    .addEventListener("click", function () {
-        gtag("event", "download-counter", { debug_mode: true });
-    });
-
-document
-    .getElementsByClassName("slides-counter")
-    .addEventListener("click", function () {
-        gtag("event", "slides-counter", { debug_mode: true });
-    });
-
-document
-    .getElementsByClassName("youtube-counter")
-    .addEventListener("click", function () {
-        gtag("event", "youtube-counter", { debug_mode: true });
-    });
-
-document
-    .getElementsByClassName("lifekids-counter")
-    .addEventListener("click", function () {
-        gtag("event", "lifekids-counter", { debug_mode: true });
-    });
