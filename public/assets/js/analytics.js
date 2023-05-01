64ac95280a9638d2bc69d7e4253757524af7a68e
magnanimox <tiagoeds96@gmail.com>
2023-05-01 16:01:53 -0300

    debug mod test

--- public/assets/js/analytics.js
+++ public/assets/js/analytics.js
@@ -1,23 +1,47 @@
-document.getElementById("ab-download").addEventListener("click", function () {
-    gtag("event", "ab-download-counter");
-});
+function Ab_Download_Counter() {
+    gtag("event", "ab-download-counter", { "debug-mode": true });
+}
 
-document.getElementById("ab-materiais").addEventListener("click", function () {
-    gtag("event", "ab-download-counter");
-});
+function Ab_Materiais_Counter() {
+    gtag("event", "ab-materiais-counter");
+}
 
-document.getElementById("tp-download").addEventListener("click", function () {
+function Tp_Download_Counter() {
     gtag("event", "tp-download-counter");
-});
+}
 
-document.getElementById("tp-slides").addEventListener("click", function () {
+function Tp_Slides_Counter() {
     gtag("event", "tp-slides-counter");
-});
+}
 
-document.getElementById("tp-youtube").addEventListener("click", function () {
+function Tp_Youtube_Counter() {
     gtag("event", "tp-youtube-counter");
-});
+}
 
-document.getElementById("tp-lifekids").addEventListener("click", function () {
+function Tp_Lifekids_Counter() {
     gtag("event", "tp-lifekids-counter");
-});
+}
+
+// document.getElementById("ab-download").addEventListener("click", function () {
+//     gtag("event", "ab-download-counter");
+// });
+
+// document.getElementById("ab-materiais").addEventListener("click", function () {
+//     gtag("event", "ab-download-counter");
+// });
+
+// document.getElementById("tp-download").addEventListener("click", function () {
+//     gtag("event", "tp-download-counter");
+// });
+
+// document.getElementById("tp-slides").addEventListener("click", function () {
+//     gtag("event", "tp-slides-counter");
+// });
+
+// document.getElementById("tp-youtube").addEventListener("click", function () {
+//     gtag("event", "tp-youtube-counter");
+// });
+
+// document.getElementById("tp-lifekids").addEventListener("click", function () {
+//     gtag("event", "tp-lifekids-counter");
+// });
