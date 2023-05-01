719ebd33049c768dc5d00e84c8e6a1b503bc4689
magnanimox <tiagoeds96@gmail.com>
2023-05-01 14:37:26 -0300

    analityc debug mode

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -89,3 +89,27 @@ function closeModal() {
         c(".modal-login").style.display = "none";
     }, 500);
 }
+
+document
+    .getElementsByClassName("download-counter")
+    .addEventListener("click", function () {
+        gtag("event", "download-counter", { debug_mode: true });
+    });
+
+document
+    .getElementsByClassName("slides-counter")
+    .addEventListener("click", function () {
+        gtag("event", "slides-counter", { debug_mode: true });
+    });
+
+document
+    .getElementsByClassName("youtube-counter")
+    .addEventListener("click", function () {
+        gtag("event", "youtube-counter", { debug_mode: true });
+    });
+
+document
+    .getElementsByClassName("lifekids-counter")
+    .addEventListener("click", function () {
+        gtag("event", "lifekids-counter", { debug_mode: true });
+    });
