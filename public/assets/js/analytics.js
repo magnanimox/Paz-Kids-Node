dd2b3c92ff2d14f14e5cc3f3dbb84bdbfff966fe
magnanimox <tiagoeds96@gmail.com>
2023-05-01 17:23:46 -0300

    debug mod test

--- public/assets/js/analytics.js
+++ public/assets/js/analytics.js
@@ -1,26 +1,26 @@
-window.ab_Download_Counter = function () {
-    gtag("event", "ab-download-counter", { "debug-mode": true });
-}
+// window.ab_Download_Counter = function () {
+//     gtag("event", "ab-download-counter");
+// }
 
-window.ab_Materiais_Counter = function() {
-    gtag("event", "ab-materiais-counter", { "debug-mode": true });
-}
+// window.ab_Materiais_Counter = function() {
+//     gtag("event", "ab-materiais-counter", { "debug-mode": true });
+// }
 
-window.tp_Download_Counter() = function() {
-    gtag("event", "tp-download-counter", { "debug-mode": true });
-}
+// window.tp_Download_Counter() = function() {
+//     gtag("event", "tp-download-counter", { "debug-mode": true });
+// }
 
-window.tp_Slides_Counter() = function() {
-    gtag("event", "tp-slides-counter", { "debug-mode": true });
-}
+// window.tp_Slides_Counter() = function() {
+//     gtag("event", "tp-slides-counter", { "debug-mode": true });
+// }
 
-window.tp_Youtube_Counter() = function() {
-    gtag("event", "tp-youtube-counter", { "debug-mode": true });
-}
+// window.tp_Youtube_Counter() = function() {
+//     gtag("event", "tp-youtube-counter", { "debug-mode": true });
+// }
 
-window.tp_Lifekids_Counter() = function() {
-    gtag("event", "tp-lifekids-counter", { "debug-mode": true });
-}
+// window.tp_Lifekids_Counter() = function() {
+//     gtag("event", "tp-lifekids-counter", { "debug-mode": true });
+// }
 
 // document.getElementById("ab-download").addEventListener("click", function () {
 //     gtag("event", "ab-download-counter");
