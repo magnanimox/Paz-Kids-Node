1e8d79a5c273d6ebf276b21f2e87148953d8b80b
magnanimox <tiagoeds96@gmail.com>
2023-05-01 15:00:45 -0300

    analityc debug mode 4

--- public/assets/js/analytics.js
+++ public/assets/js/analytics.js
@@ -1,23 +1,15 @@
-document
-    .getElementsByClassName("download-counter")
-    .addEventListener("click", function () {
-        gtag("event", "download-counter", { debug_mode: true });
-    });
+document.getElementById("tp-download").addEventListener("click", function () {
+    gtag("event", "download-counter", { debug_mode: true });
+});
 
-document
-    .getElementsByClassName("slides-counter")
-    .addEventListener("click", function () {
-        gtag("event", "slides-counter", { debug_mode: true });
-    });
+document.getElementById("tp-slides").addEventListener("click", function () {
+    gtag("event", "slides-counter", { debug_mode: true });
+});
 
-document
-    .getElementsByClassName("youtube-counter")
-    .addEventListener("click", function () {
-        gtag("event", "youtube-counter", { debug_mode: true });
-    });
+document.getElementById("tp-youtube").addEventListener("click", function () {
+    gtag("event", "youtube-counter", { debug_mode: true });
+});
 
-document
-    .getElementsByClassName("lifekids-counter")
-    .addEventListener("click", function () {
-        gtag("event", "lifekids-counter", { debug_mode: true });
-    });
+document.getElementById("tp-lifekids").addEventListener("click", function () {
+    gtag("event", "lifekids-counter", { debug_mode: true });
+});
