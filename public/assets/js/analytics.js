80f6b7bc2d01d4112537e3ecf654c5527c233ac6
magnanimox <tiagoeds96@gmail.com>
2023-05-04 11:14:37 -0300

    thumb especial mae

--- public/assets/js/analytics.js
+++ public/assets/js/analytics.js
@@ -1,23 +1,23 @@
 document.getElementById("ab-download").addEventListener("click", function () {
-    gtag("event", "ab-download-counter", { debug_mode: true });
+    gtag("event", "ab-download-counter");
 });
 
 document.getElementById("ab-materiais").addEventListener("click", function () {
-    gtag("event", "ab-download-counter", { debug_mode: true });
+    gtag("event", "ab-download-counter");
 });
 
 document.getElementById("tp-download").addEventListener("click", function () {
-    gtag("event", "tp-download-counter", { debug_mode: true });
+    gtag("event", "tp-download-counter");
 });
 
 document.getElementById("tp-slides").addEventListener("click", function () {
-    gtag("event", "tp-slides-counter", { debug_mode: true });
+    gtag("event", "tp-slides-counter");
 });
 
 document.getElementById("tp-youtube").addEventListener("click", function () {
-    gtag("event", "tp-youtube-counter", { debug_mode: true });
+    gtag("event", "tp-youtube-counter");
 });
 
 document.getElementById("tp-lifekids").addEventListener("click", function () {
-    gtag("event", "tp-lifekids-counter", { debug_mode: true });
+    gtag("event", "tp-lifekids-counter");
 });
