c7abadb3c6eac04a8ef7f922d73c76a6415651b4
Tiago Elias de Sá <tiagodesa@Tiagos-MacBook-Pro-2.local>
2024-02-20 20:08:22 -0300

    acho que vai 3

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -3,7 +3,7 @@ const MySQLStore = require("express-mysql-session")(session);
 
 const options = {
     host: process.env.MYSQL_HOST as string,
-    port: parseInt(process.env.MYSQL_PORT as string) || 3306,
+    port: parseInt(process.env.MYSQL_PORT as string) || "3306",
     user: process.env.MYSQL_USER as string,
     password: process.env.MYSQL_PASSWORD as string,
     database: process.env.MYSQL_DATABASE as string,
