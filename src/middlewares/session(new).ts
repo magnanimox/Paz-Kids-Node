2aa9a07b3a2aaf716ba9198f377c1a07eb6bf401
Tiago Elias de Sá <tiagodesa@Tiagos-MacBook-Pro-2.local>
2024-02-20 19:49:40 -0300

    back

---
+++ src/middlewares/session(new).ts
@@ -0,0 +1,31 @@
+import session from "express-session";
+import dotenv from "dotenv";
+
+dotenv.config({ 
+    path: './.env.production' 
+});
+
+const MySQLStore = require("express-mysql-session")(session);
+
+const options = {
+    host: process.env.MYSQL_HOST as string,
+    port: parseInt(process.env.MYSQL_PORT as string) || 3306,
+    user: process.env.MYSQL_USER as string,
+    password: process.env.MYSQL_PASSWORD as string,
+    database: process.env.MYSQL_DATABASE as string,
+    clearExpired: true,
+    checkExpirationInterval: 900000,
+};
+
+const sessionStore = new MySQLStore(options);
+
+export const sessionConfigs = session({
+    name: "pazkids-session",
+    cookie: {
+        path: "/",
+        maxAge: 3600000 * 500,
+    },
+    store: sessionStore,
+    secret: "$2y$10$EyRoSbN1wRqLrEQV9TzR0uk8w/TZX1DdyO7wGYW2eWhSVuW2DbmEG",
+    saveUninitialized:false
+});
